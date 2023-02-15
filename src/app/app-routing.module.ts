91b32026ac902049558c7c7c75bd273f7247eddb
MatejGalic <matey.galic99@gmail.com>
2023-02-15 18:30:10 +0100

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './components/pages/about/about.component';
-import { ApartmentsAneComponent } from './components/pages/apartments-ane/apartments-ane.component';
-import { CocktailsComponent } from './components/pages/cocktails/cocktails.component';
-import { ColnaComponent } from './components/pages/colna/colna.component';
-import { DinersComponent } from './components/pages/diners/diners.component';
-import { ErsteComponent } from './components/pages/erste/erste.component';
-import { HomepageComponent } from './components/pages/homepage/homepage.component';
-import { LavanderaComponent } from './components/pages/lavandera/lavandera.component';
-import { PodravkaComponent } from './components/pages/podravka/podravka.component';
-import { VegetaComponent } from './components/pages/vegeta/vegeta.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ApartmentsAneComponent } from './pages/apartments-ane/apartments-ane.component';
+import { CocktailsComponent } from './pages/cocktails/cocktails.component';
+import { ColnaComponent } from './pages/colna/colna.component';
+import { DinersComponent } from './pages/diners/diners.component';
+import { ErsteComponent } from './pages/erste/erste.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { LavanderaComponent } from './pages/lavandera/lavandera.component';
+import { PodravkaComponent } from './pages/podravka/podravka.component';
+import { VegetaComponent } from './pages/vegeta/vegeta.component';
 
 const routes: Routes = [
   {
